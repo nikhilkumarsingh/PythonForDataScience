9662290e7f3fe0c444acbab4440a8b67fa873277
Gaurav Rawal <31179124+black0405@users.noreply.github.com>
2018-06-25 11:24:27 +0530

    Update pattern.py
    Checking for odd number

--- pattern.py
+++ pattern.py
@@ -1,6 +1,9 @@
-n = int(input("Enter a odd number for a beautiful pattern: "))
-for i in range(n):
-   for j in range(n):
-     print(n-min(i,j,n-1-i,n-1-j),end="")
-   print()
+n = int(input("Enter an odd number for a beautiful pattern: "))
+if n%2 != 0:
+  for i in range(n):
+      for j in range(n):
+         print(n-min(i,j,n-1-i,n-1-j),end="")
+      print()
+else
+  print("Not an odd number")
  
