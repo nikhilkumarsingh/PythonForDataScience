bde4345ad6d4914b84618884445c4c8e220f8740
Gaurav Rawal <31179124+black0405@users.noreply.github.com>
2018-06-25 11:20:21 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,4 +1,4 @@
-n = int(input("Enter a odd number for a beautiful pattern: ")
+n = int(input("Enter a odd number for a beautiful pattern: "))
 for i in range(n):
    for j in range(n):
      print(n-min(i,j,n-1-i,n-1-j),end="")
